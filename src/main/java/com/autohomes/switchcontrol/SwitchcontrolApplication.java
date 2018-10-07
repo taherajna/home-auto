4df076a3066afc6437d0eacfb94507c26497fc7b
taherajna <Tmnagitb786!>
2018-10-07 11:37:48 +0530

    Converted project to spring boot web app

---
+++ src/main/java/com/autohomes/switchcontrol/SwitchcontrolApplication.java
@@ -0,0 +1,12 @@
+package com.autohomes.switchcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchcontrolApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwitchcontrolApplication.class, args);
+  }
+}
